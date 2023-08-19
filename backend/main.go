f89032055cdff8aff0dc6e67b441ef69451a5f7b
Tal Kohavy <talkohavy@gmail.com>
2023-08-19 11:33:59 +0300

    master: go server fetches organization's repositories

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	cors "github.com/itsjamie/gin-cors"
 )
+
+const (
+	reposURL       = "https://api.github.com/orgs/sysdig/repos"
+)
+
+type Repository struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	HTMLURL     string `json:"html_url"`
+}
 
 func main() {
 
@@ -21,9 +35,36 @@ func main() {
 
 	// Get Repos Route
 	router.GET("/repositories", func(c *gin.Context) {
-		// TODO: Add the functionality to query GitHub to get the organization's repositories
+
+		resp, err := http.Get(fmt.Sprint(reposURL))
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch repositories"})
+			return
+		}
+
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch repositories"})
+			return
+		}
+
+		var repositories []Repository
+
+		err = json.NewDecoder(resp.Body).Decode(&repositories)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode response"})
+			return
+		}
+
+		c.JSON(http.StatusOK, repositories)
 	})
 
-	router.Run("localhost:4200")
+	// Get Repos Route
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK,"Hello World!")
+	})
 
+	router.Run("localhost:8000")
 }
